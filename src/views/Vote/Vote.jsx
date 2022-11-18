679afb32524074f273d46ded0a07b9b97192dd37
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-18 14:52:05 -0400

    vote

---
+++ src/views/Vote/Vote.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GameDescription from '../../components/GameDescription/GameDescription'
+
+
+const Vote = () => {
+  return (
+    <>
+        <GameDescription />
+    </>
+  )
+}
+
+export default Vote
