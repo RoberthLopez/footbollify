2958fb87d5dad79fc6edfd549db0138351a7c000
Ezequiel Zezé Breiterman <32859091+ZezeBreiterman@users.noreply.github.com>
2022-11-18 19:03:30 -0300

    Creacion de cartas en estadisticas, css agregados

--- src/views/MyStats/MyStats.jsx
+++ src/views/MyStats/MyStats.jsx
@@ -1,62 +1,50 @@
-import React, {useState} from 'react'
-import { PieChart } from '../../components/PieChart/PieChart'
-import './MyStats.css'
+import React, { useState } from "react";
+import { PieChart } from "../../components/PieChart/PieChart";
+import "./MyStats.css";
+import MyStatsCard from "./MyStatsCard";
 
 const MyStats = () => {
-    const usuarioData = [
-        {
-          id: 1,
-          name: "Apuestas Ganadas",
-          qty: 10,
-        },
-        {
-          id: 2,
-          name: "Apuestas Perdidas",
-          qty: 20,
-        },
-      ];
-    const [userData, setUserData] = useState({
-        labels: usuarioData.map((data) => data.name),
-        datasets: [
-          {            
-            data: usuarioData.map((data) => data.qty),
-            backgroundColor: [
-                "#6FCDCD",
-              "#FF829D"
-            ],
-            borderColor: "white",
-            borderWidth: 2,
-          },
-        ],
-      });
+  const usuarioData = [
+    {
+      id: 1,
+      name: "Apuestas Ganadas",
+      qty: 10,
+    },
+    {
+      id: 2,
+      name: "Apuestas Perdidas",
+      qty: 20,
+    },
+  ];
+  const [userData, setUserData] = useState({
+    labels: usuarioData.map((data) => data.name),
+    datasets: [
+      {
+        data: usuarioData.map((data) => data.qty),
+        backgroundColor: ["#6FCDCD", "#FF829D"],
+        borderColor: "white",
+        borderWidth: 2,
+      },
+    ],
+  });
   return (
     <>
-        <div className='mystats__container'>
-            <div className='mystats__stats'>
-                <div className='mystats__title'>
-                    Apuestas:
-                </div>
-                <div>
-                    <div>
-                        Cantidad: 30
-                    </div>
-                    <div>
-                        Ganadas: 25
-                    </div>
-                    <div>
-                        Perdidas: 50
-                    </div>
-                    <div>
-                        Puntos Acumulados: 50
-                    </div>
-                </div>
-            </div>
-            <div className='chart__container'>
-                <PieChart  chartData={userData} />
-            </div>
+      <div className="mystats__container">
+        <div className="mystats__stats">
+          <div className="mystats__title">Apuestas</div>
+          <div className="mystats__cards">
+            <MyStatsCard title={"Cantidad"} qnty={75} />
+            <MyStatsCard title={"Ganadas"} qnty={25} />
+            <MyStatsCard title={"Perdidas"} qnty={50} />
+            <MyStatsCard title={"Puntos Acumulados"} qnty={50} />
+          </div>
         </div>
+        <div className="chart__container">
+          <PieChart chartData={userData} />
+        </div>
+      </div>
     </>
-  )
-}
+  ); 
+};
 
-export default MyStats
+export default MyStats;
