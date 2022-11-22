275481d1ec619aeffc1bf4fd39ea37f015fa5f63
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-22 16:20:18 -0400

    fixed timing to show next matchs

--- src/hooks/useGetMatchs.js
+++ src/hooks/useGetMatchs.js
@@ -1,12 +1,12 @@
 import { useEffect,useState } from "react";
 import axios from 'axios';
 
-const useGetGames = (API) => {
-    const [games, setGames]=useState([]);
+const useGetMatchs = (API) => {
+    const [matchs, setMatchs]=useState([]);
     const fetchData = async() => {
         try {
             const response = await axios.get(API); //request
-            setGames(response.data);                
+            setMatchs(response.data.data);               
         } catch (error) {
             console.error(error)
         }
@@ -14,7 +14,7 @@ const useGetGames = (API) => {
 	useEffect(() => {
         fetchData();
     }, []);
-  return games;
+  return matchs;
 }
 
-export default useGetGames
+export default useGetMatchs
--- src/views/NextGames/NextGames.jsx
+++ src/views/NextGames/NextGames.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import GameList from '../../components/GameList/GameList'
-import useGetGames from '../../hooks/useGetNextGames';
+import useGetMatchs from '../../hooks/useGetMatchs';
 
-const API='/matchs/bymatchday';
-
+const API='/matchs';
 const NextGames = () => {
-  const games = useGetGames(API)
+  const games = useGetMatchs(API)
+  
+  let obj = {};
+  for (let data of games) {
+    // if empty, creates a array
+    obj[data.matchday] = obj[data.matchday] || [];
+    // add data in that array
+    obj[data.matchday].push(data);
+  }
+
+  useEffect(() => {
+    console.log(Object.values(obj))
+  }, [])
+
   return (
     <>
-        {games.map((matchs, i)=>(
+        {Object.values(obj).map((matchs, i)=>(
 					<GameList matchs={matchs} key={i}/>
 				))}
     </>
