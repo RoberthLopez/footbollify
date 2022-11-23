2becea2d2985e0d942b14ff9d069090a3596da60
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-22 20:13:39 -0400

    In next games, showing only no finished ones

--- src/views/NextGames/NextGames.jsx
+++ src/views/NextGames/NextGames.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import GameList from '../../components/GameList/GameList'
 import useGetMatchs from '../../hooks/useGetMatchs';
 
@@ -6,8 +6,16 @@ const API='/matchs';
 const NextGames = () => {
   const games = useGetMatchs(API)
   
-  let obj = {};
+  let obj2 = {};
   for (let data of games) {
+    // if empty, creates a array
+    obj2[data.finished] = obj2[data.finished] || [];
+    // add data in that array
+    obj2[data.finished].push(data);
+  }
+
+  let obj = {};
+  for (let data of obj2['FALSE']) {
     // if empty, creates a array
     obj[data.matchday] = obj[data.matchday] || [];
     // add data in that array
